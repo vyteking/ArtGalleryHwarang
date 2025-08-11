8ea5ab465b9638a791a1101101be1b44dbe0843f
vyteking <richser133@gmail.com>
2025-08-11 13:03:59 +0900

    Frame for personal page formed

--- ArtGalleryHwarangReactFrontend/src/userinfo/personalpage.jsx
+++ ArtGalleryHwarangReactFrontend/src/userinfo/personalpage.jsx
@@ -8,7 +8,23 @@ function LoadPersonalInfo() {
 function PersonalPage(userindex1st) {
     return (
         <div id="PersonalPage" className={GetClassNames("layout")}>
+            <div id="Profile">
+                <div id="ProfilePic">
 
+                </div>
+                <div id="UserName">
+                    nickname (Index1st)
+                </div>
+                <div id="Following">
+                    following
+                </div>
+                <div id="Followers">
+                    followers
+                </div>
+            </div>
+            <div id="UserGallery">
+
+            </div>
         </div>
     );
 }
